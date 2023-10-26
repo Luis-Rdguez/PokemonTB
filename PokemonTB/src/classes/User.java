32c6fba3abfe4509cfe2770fbc57f88122ab35f3
oierga <oier.garcia@opendeusto.es>
2023-10-26 19:41:36 +0200

    clase user

--- PokemonTB/src/classes/User.java
+++ PokemonTB/src/classes/User.java
@@ -2,4 +2,62 @@ package classes;
 
 public class User {
 
+	private String username;
+	private String password;
+	private String firstSurname;
+	private String secondSurname;
+	private String email;
+	private int telephone;
+	public User(String username, String password, String firstSurname, String secondSurname, String email,
+			int telephone) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstSurname = firstSurname;
+		this.secondSurname = secondSurname;
+		this.email = email;
+		this.telephone = telephone;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstSurname() {
+		return firstSurname;
+	}
+	public void setFirstSurname(String firstSurname) {
+		this.firstSurname = firstSurname;
+	}
+	public String getSecondSurname() {
+		return secondSurname;
+	}
+	public void setSecondSurname(String secondSurname) {
+		this.secondSurname = secondSurname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(int telephone) {
+		this.telephone = telephone;
+	}
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", firstSurname=" + firstSurname
+				+ ", secondSurname=" + secondSurname + ", email=" + email + ", telephone=" + telephone + "]";
+	}
+	
 }
